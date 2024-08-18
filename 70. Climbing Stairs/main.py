30e0e292a2e7492af8d1a63b5c3830d2889b0d6e
ilyas <ilyas.dev2021@gmail.com>
2024-08-18 23:55:17 +0500

    70. Climbing Stairs

---
+++ 70. Climbing Stairs/main.py
@@ -0,0 +1,6 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # will excede time limit
+        if n<=1:
+            return 1
+        return climbStairs(n-1) + climbStairs(n-2)
